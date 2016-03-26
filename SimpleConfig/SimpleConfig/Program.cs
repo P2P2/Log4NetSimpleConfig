2a6ada92a2787f912a3fdfd20773d4bdc67a73dc
Paul Filipski <paul.filipski@gmail.com>
2016-03-26 10:06:11 -0400

    Add example configuration for file and console log output

--- SimpleConfig/SimpleConfig/Program.cs
+++ SimpleConfig/SimpleConfig/Program.cs
@@ -9,9 +9,11 @@ namespace SimpleConfig
         static void Main(string[] args)
         {
             //ConfigureLoggingToConsole();
-            ConfigureLoggingToFile(@"C:\temp\log.txt");
+            //ConfigureLoggingToFile(@"C:\temp\log.txt");
+            ConfigureLoggingToFileAndConsole(@"C:\temp\log.txt");
 
             log.Info("Hello, world.");
+            log.Debug("I like saying hello.");
         }
 
         private static void ConfigureLoggingToConsole()
@@ -53,6 +55,42 @@ namespace SimpleConfig
             appender.ActivateOptions();
 
             log4net.Config.BasicConfigurator.Configure(appender);
+        }
+
+        private static void ConfigureLoggingToFileAndConsole(string fileName)
+        {
+            var fileLayout = new log4net.Layout.PatternLayout
+            {
+                ConversionPattern = @"%date{HH:mm:ss.fff} %8timestamp %-5level [%thread] |%ndc| %logger{2} %message%newline%exception",
+                Header = new log4net.Util.PatternString(@"%appdomain run by %username at %date{HH:mm:ss.fff}%newline").Format(),
+                Footer = new string('-', 80) + Environment.NewLine
+            };
+            fileLayout.ActivateOptions();
+
+            var fileAppender = new log4net.Appender.RollingFileAppender
+            {
+                AppendToFile = true,
+                CountDirection = 1,
+                File = fileName,
+                Layout = fileLayout,
+                MaximumFileSize = "200MB",
+                MaxSizeRollBackups = 20,
+                RollingStyle = log4net.Appender.RollingFileAppender.RollingMode.Size,
+                Threshold = log4net.Core.Level.Debug
+            };
+            fileAppender.ActivateOptions();
+
+            var consoleLayout = new log4net.Layout.PatternLayout(@"%date{HH:mm:ss.fff} %8timestamp %-5level [%thread] |%ndc| %logger{2} %message%newline%exception");
+            consoleLayout.ActivateOptions();
+
+            var consoleAppender = new log4net.Appender.ConsoleAppender
+            {
+                Layout = consoleLayout,
+                Threshold = log4net.Core.Level.Info
+            };
+            consoleAppender.ActivateOptions();
+
+            log4net.Config.BasicConfigurator.Configure(fileAppender, consoleAppender);
         }
     }
 }
